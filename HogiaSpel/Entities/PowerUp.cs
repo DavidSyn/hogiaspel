d273c2854a0b665d09380dfb0a27a69162e21f3b
Hemma\Daniel <d.o.rasmusson@gmail.com>
2018-02-25 13:24:29 +0100

    Added PowerUp entity

---
+++ HogiaSpel/Entities/PowerUp.cs
@@ -0,0 +1,40 @@
+﻿using HogiaSpel.Enums;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogiaSpel.Entities
+{
+    class PowerUp : AbstractEntity, IEntity
+    {
+        public void Initialize(Vector2 position)
+        {
+            Id = Guid.NewGuid();
+            Active = true;
+            Speed = 0;
+            BaseSpeed = 0;
+            TopSpeed = 0;
+            Acceleration = 0;
+            CurrentAccelerationDirection = DirectionEnum.NoDirection;
+
+            var sprites = Sprites.Instance;
+            SpriteHandler = new SpriteHandler(position);
+            SpriteHandler.InitializeAnimation(SpriteKeys.PowerUp.Stand, sprites.GetSprite(SpriteKeys.PowerUp.Stand), 64, 64, 1, 80, Color.White, 1f, true);
+            SpriteHandler.Initialize(SpriteKeys.PowerUp.Stand);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            SpriteHandler.Draw(spriteBatch);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            SpriteHandler.Update(gameTime);
+        }
+    }
+}
